64546bd2e125eedc92f70b5744a4ceefe262da9d
kirillsnovikov <kirillsnovikov@gmail.com>
2024-07-16 00:50:01 +0300

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    modules: {
+      localsConvention: 'camelCaseOnly',
+    },
+  },
+  resolve: {
+    alias: {
+      '@src': '/src',
+      '@shared': '/src/shared',
+      '@public': '',
+    },
+  },
 });
